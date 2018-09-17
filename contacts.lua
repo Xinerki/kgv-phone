77493482f911c1c9926dc8831643bdb980e901f5
Xinerki <simasadves@gmail.com>
2018-09-17 23:08:04 +0300

    custom ytd test, as well as png test

--- contacts.lua
+++ contacts.lua
@@ -1,11 +1,17 @@
 
 -- SetContactRaw(GlobalScaleform, 0, "Xin Voliteer", "CHAR_DEFAULT")
 
+-- local txd = CreateRuntimeTxd('CHAR_PNGTEST')
+-- local duiObj = CreateDui('https://i.imgur.com/NZaICKc.png', 64, 64)
+-- _G.duiObj = duiObj
+-- local dui = GetDuiHandle(duiObj)
+-- local tx = CreateRuntimeTextureFromDuiHandle(txd, 'CHAR_PNGTEST', dui)
+
 contacts = {
-	-- XinVoliteer = {
-		-- name = "Xin Voliteer", 
-		-- icon = "CHAR_DEFAULT",
-	-- },
+	XinVoliteer = {
+		name = "Xin Voliteer", 
+		icon = "CHAR_XIN",
+	},
 	-- MattLaurence = {
 		-- name = "Matthew Laurence", 
 		-- icon = "CHAR_DEFAULT",
@@ -26,4 +32,8 @@ contacts = {
 		name = "Lester", 
 		icon = "CHAR_LESTER",
 	},
+	-- PNG_TEST = {
+		-- name = "PNG_TEST", 
+		-- icon = "CHAR_PNGTEST",
+	-- },
 }
