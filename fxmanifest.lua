da572d39ca7d02b8e641cc75d10cd0460632ba2b
SuperDaniel <56658457+TransitNode@users.noreply.github.com>
2020-01-29 16:05:07 +0100

    Replace deprecated __resource.lua file with new manifest standard.
    Replaces __resource.lua with fxmanifest.lua.

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+name 'kgv-phone'
+description 'A vanilla phone resource built on scaleforms.'
+author 'Xinerki - https://github.com/Xinerki/'
+url 'https://github.com/Xinerki/kgv-phone'
+
+client_scripts {
+    'client.lua',
+    'contacts.lua',
+	'cl_functions.lua'
+}
+
+server_script 'server.lua'
+
